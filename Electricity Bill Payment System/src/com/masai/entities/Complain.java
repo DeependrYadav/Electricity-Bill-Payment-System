e9f8e9f2e8de8766059da50314d3bf70504ca3c0
Deependra Yadav <deependrayadav6306@gmail.com>
2023-05-06 22:51:11 +0530

    ComplainFeatureIsCompleted

--- Electricity Bill Payment System/src/com/masai/entities/Complain.java
+++ Electricity Bill Payment System/src/com/masai/entities/Complain.java
@@ -1,19 +1,32 @@
 package com.masai.entities;
 
-public class Complain {
+import java.io.Serializable;
+
+public class Complain implements Serializable {
 	private final int complainId;
 	private final String consumerName;
 	private final String consumerEmail;
 	private String discription;
+	private String status;
 
-	public Complain(int complainId, String consumerName, String consumerEmail, String discription) {
+
+	public Complain(int complainId, String consumerName, String consumerEmail, String discription,String status) {
 		super();
 		this.complainId = complainId;
 		this.consumerName = consumerName;
 		this.consumerEmail = consumerEmail;
 		this.discription = discription;
+		this.status = status;
 	}
+	
 
+	public String getStatus() {
+		return status;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
 	public String getDiscription() {
 		return discription;
 	}
@@ -37,7 +50,7 @@ public class Complain {
 	@Override
 	public String toString() {
 		return "ComplainId=" + complainId + ", consumerName=" + consumerName + ", consumerEmail="
-				+ consumerEmail + ", discription=" + discription;
+				+ consumerEmail + ", discription=" + discription +", Status of complain=" + status;
 	}
 
 	
--- Electricity Bill Payment System/src/com/masai/service/ComplainServiceImpl.java
+++ Electricity Bill Payment System/src/com/masai/service/ComplainServiceImpl.java
@@ -1,5 +1,49 @@
 package com.masai.service;
 
+import java.util.Map;
+
+import com.masai.entities.Complain;
+import com.masai.exceptions.InvalidCredentialsException;
+
 public class ComplainServiceImpl implements ComplainService{
+
+	@Override
+	public void raiseComplain(Complain comp, Map<Integer,Complain> complain) {
+		// TODO Auto-generated method stub
+		int id = comp.getComplainId();
+		complain.put(id, comp);
+	}
+
+	@Override
+	public void viewComplain(int complainId, Map<Integer, Complain> complain) throws InvalidCredentialsException {
+		
+		if(complain.containsKey(complainId)) {
+//			String str = complain.get(complainId);
+//			System.out.println("kasgkjgfjksf");
+			System.out.println(complain.get(complainId));
+		}else {
+			throw new InvalidCredentialsException("Please write valid Compalin Id");
+		}
+	}
+
+	@Override
+	public void updateStatusOfComplain(int complainId, String str, Map<Integer, Complain> complain)
+			throws InvalidCredentialsException {
+		if(complain.containsKey(complainId)) {
+			complain.get(complainId).setStatus(str);
+		}else {
+			throw new InvalidCredentialsException("Please write valid Compalin Id");
+		}
+		
+	}
+
+	@Override
+	public void viewAllComplain(Map<Integer, Complain> complain) {
+		
+		for(Map.Entry<Integer, Complain> comp : complain.entrySet()) {
+			System.out.println(comp);
+		}
+		
+	}
 
 }
